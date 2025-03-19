434d1a010a02cba99ee880e70f2fb935516bbada
ashtonjo <hee576@nate.com>
2025-03-19 10:27:43 +0900

    changing my bio

--- site.config.js
+++ site.config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   profile: {
     name: "wookhee jo",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "AI developer",
     bio: "love development and music",
     email: "hee576@nate.com",
     linkedin: "wookheejo",
