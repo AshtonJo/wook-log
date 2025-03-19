a67271f91e39ac9bdd453f3ed6beb382679f4f73
ashtonjo <hee576@nate.com>
2025-03-19 11:55:11 +0900

    Revert "fix: 포폴란생성"
    This reverts commit 059ae44b8185588c8e4d9e875ac7bd88c411b570.

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 const NavBar: React.FC = () => {
   const links = [
     { id: 1, name: "About", to: "/about" },
-    { id: 2, name: "Portfolio", to: "/portfolio" },
+    { id: 2, name: "Resume", to: "/resume" },
   ]
   return (
     <StyledWrapper className="">
