11a29445ec7cb8371bcf12eef7e0a48ee001b490
ashtonjo <hee576@nate.com>
2025-07-03 21:31:13 +0900

    cusdis false

--- site.config.js
+++ site.config.js
@@ -63,7 +63,7 @@ const CONFIG = {
     },
   },
   cusdis: {
-    enable: true,
+    enable: false,
     config: {
       host: "https://cusdis.com",
       appid: "71effe85-d862-4525-b882-07333e1f74c8", // Embed Code -> data-app-id value
